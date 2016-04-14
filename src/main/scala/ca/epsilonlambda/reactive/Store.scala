95690bc2f011c24cb1865e8efaa52c3bdbbb11a2
kirill <kirill.epsilonlambda@gmail.com>
2016-04-14 03:40:40 -0400

    Add dispatch() to Store facade

--- src/main/scala/ca/epsilonlambda/reactive/Store.scala
+++ src/main/scala/ca/epsilonlambda/reactive/Store.scala
@@ -5,4 +5,5 @@ import japgolly.scalajs.react._
 abstract class Store[M](reducer: Reducer[M]) {
   def connect[P](modelToProps: M => P, dispatchToProps: (P, (AnyRef => Callback)) => P, propsToElement: P => ReactElement) : ReactComponentU[_,_,_,_]
   def applyMiddleware(args: Any*) = throw new NotImplementedError
+  def dispatch(action: AnyRef)
 }
--- src/main/scala/ca/epsilonlambda/reactive/diode/DiodeStore.scala
+++ src/main/scala/ca/epsilonlambda/reactive/diode/DiodeStore.scala
@@ -6,7 +6,11 @@ import japgolly.scalajs.react._
 
 class DiodeStore[M](reducer: Reducer[M]) extends Store[M](reducer){
   val circuit = new ReducerCircuit(reducer)
-  
+
   override def connect[P](modelToProps: M => P, dispatchToProps: (P, (AnyRef => Callback)) => P, propsToElement: P => ReactElement) : ReactComponentU[_,_,_,_] =
     circuit.connect(x=>x)((proxy) => propsToElement(dispatchToProps(modelToProps(proxy().model), proxy.dispatch)))
+
+  override def dispatch(action: AnyRef) = {
+    circuit.dispatch(action)
+  }
 }
